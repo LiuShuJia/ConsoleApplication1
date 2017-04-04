339d04885eb8ae53610038f2d7a9ec62e5a40bb2
刘树家 <WEN@DESKTOP-4ARAIDA>
2017-04-04 14:49:10 +0800

    添加了输出hello

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("an esay day");
+            Console.WriteLine("hello");
         }
     }
 }
