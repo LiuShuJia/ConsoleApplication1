bcb22283c9d9a8ad8156406612a3b8fb962a9e81
左思仪 <WEN@DESKTOP-4ARAIDA>
2017-04-04 16:50:36 +0800

    更改了输出hello语句  在hello后加了123

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("an esay day");
-            Console.WriteLine("hello");
+            Console.WriteLine("hello 123");
             Console.WriteLine("你好");
         }
     }
